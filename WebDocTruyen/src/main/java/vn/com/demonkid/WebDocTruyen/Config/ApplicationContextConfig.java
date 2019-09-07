9ea706c740da70e269cc86b7165bcbd34ce34426
Toàn NC6 <tn.toannguyen2811@gmail.com>
2019-09-07 15:34:08 +0700

    Website doc truyen

---
+++ WebDocTruyen/src/main/java/vn/com/demonkid/WebDocTruyen/Config/ApplicationContextConfig.java
@@ -0,0 +1,19 @@
+package vn.com.demonkid.WebDocTruyen.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("vn.com.demonkid.WebDocTruyen.*")
+public class ApplicationContextConfig {
+	@Bean(name = "viewResolver")
+	public InternalResourceViewResolver getViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+}
